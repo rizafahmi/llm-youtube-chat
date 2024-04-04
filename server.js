c70c28b371e683f456640d66762f1e3a23b17b32
Riza Fahmi <rizafahmi@gmail.com>
2024-04-04 14:40:44 +0700

    Add chat functionality to server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import http from 'http';
 
+async function invoke(chat) {
+  console.log(chat);
+  return 'Hello from LLM';
+}
+
 async function handler(req, res) {
   const { url } = req;
   console.log(`Handling ${url}...`);
   if (url === '/health') {
     res.writeHead(200).end('OK');
+  } else if (url.startsWith('/chat')) {
+    const parsedUrl = new URL(`http://localhost/${url}`);
+    const { search } = parsedUrl;
+    const chat = decodeURIComponent(search.substring(1));
+    console.log({ chat });
+    console.log('Waiting for LLM to response...');
+    const result = await invoke(chat);
+    res.writeHead(200).end(result);
   } else {
     console.error(`${url} not found`);
     res.writeHead(404);
