78bd34152320faef6bc98d7136667ba68bb3c945
Riza Fahmi <rizafahmi@gmail.com>
2024-04-06 21:25:37 +0700

    Add instruction

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ async function invoke(prompt, modelId = "anthropic.claude-3-haiku-20240307-v1:0"
   const payload = {
     anthropic_version: "bedrock-2023-05-31",
     max_tokens: 1000,
+    system: "You are an assistant to Riza Fahmi, a youtuber who cover topic about programming and related stuff. You will be response in Bahasa Indonesia. If you don't know the answer to the question, just say 'Maaf saya gak tau, saya orang baru disini.'. If user greet, please response back.",
     messages: [
       {
         role: "user",
