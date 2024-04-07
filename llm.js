6a9e8827416159f512cd641671b9ccce5e654d50
Riza Fahmi <rizafahmi@gmail.com>
2024-04-07 15:18:51 +0700

    Modularize invoke function

---
+++ llm.js
@@ -0,0 +1,34 @@
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand
+} from "@aws-sdk/client-bedrock-runtime";
+
+export async function invoke(prompt, modelId = "anthropic.claude-3-haiku-20240307-v1:0") {
+  const client = new BedrockRuntimeClient({ region: "us-east-1" });
+
+  const payload = {
+    anthropic_version: "bedrock-2023-05-31",
+    max_tokens: 1000,
+    system: "You are an assistant to Riza Fahmi, a youtuber who cover topic about programming and related stuff. You will be response in Bahasa Indonesia. Your response should be less than 22 words. If you don't know the answer to the question, just say 'Maaf saya gak tau, saya orang baru disini.'. If user greet, please response back.",
+    messages: [
+      {
+        role: "user",
+        content: [{ type: "text", text: prompt }],
+      },
+    ],
+  };
+
+  const command = new InvokeModelCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId,
+  });
+  const apiResponse = await client.send(command);
+
+  // Decode and return the response(s)
+  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+
+  const responseBody = JSON.parse(decodedResponseBody);
+  return responseBody.content[0].text;
+
+}
--- server.js
+++ server.js
@@ -1,40 +1,6 @@
 import http from 'http';
 import { fileURLToPath } from "url";
-
-import {
-  BedrockRuntimeClient,
-  InvokeModelCommand
-} from "@aws-sdk/client-bedrock-runtime";
-
-async function invoke(prompt, modelId = "anthropic.claude-3-haiku-20240307-v1:0") {
-  const client = new BedrockRuntimeClient({ region: "us-east-1" });
-
-  const payload = {
-    anthropic_version: "bedrock-2023-05-31",
-    max_tokens: 1000,
-    system: "You are an assistant to Riza Fahmi, a youtuber who cover topic about programming and related stuff. You will be response in Bahasa Indonesia. If you don't know the answer to the question, just say 'Maaf saya gak tau, saya orang baru disini.'. If user greet, please response back.",
-    messages: [
-      {
-        role: "user",
-        content: [{ type: "text", text: prompt }],
-      },
-    ],
-  };
-
-  const command = new InvokeModelCommand({
-    contentType: "application/json",
-    body: JSON.stringify(payload),
-    modelId,
-  });
-  const apiResponse = await client.send(command);
-
-  // Decode and return the response(s)
-  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
-
-  const responseBody = JSON.parse(decodedResponseBody);
-  return responseBody.content[0].text;
-
-}
+import { invoke } from "./llm.js";
 
 async function handler(req, res) {
   const { url } = req;
